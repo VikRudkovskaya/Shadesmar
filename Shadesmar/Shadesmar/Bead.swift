c5ce243dcb570f384183ee94dc21edb4349e080f
VikRudkovskaya <vik-rudkovskaya@yandex.ru>
2019-04-04 11:28:52 +0300

    Add. Project

---
+++ Shadesmar/Shadesmar/Bead.swift
@@ -0,0 +1,9 @@
+//
+//  Bead.swift
+//  Shadesmar
+//
+//  Created by Viktoria Rudkovskaya on 04/04/2019.
+//  Copyright © 2019 Viktoria Rudkovskaya. All rights reserved.
+//
+
+import Foundation
---
+++ Shadesmar/Shadesmar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shadesmar
+//
+//  Created by Viktoria Rudkovskaya on 03/04/2019.
+//  Copyright © 2019 Viktoria Rudkovskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
