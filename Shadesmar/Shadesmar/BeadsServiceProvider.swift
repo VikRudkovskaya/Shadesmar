7b7c489b329dc6c6fc80cb01ebd8ce52bf506570
VikRudkovskaya <vik-rudkovskaya@yandex.ru>
2019-06-25 14:01:43 +0300

    Refactoring. Вынос общего функционала

--- Shadesmar/Shadesmar/BeadsServiceProvider.swift
+++ Shadesmar/Shadesmar/BeadsServiceProvider.swift
@@ -16,47 +16,47 @@ class BeadsServiceProvider {
     
     var allBeadProperties: [BeadProperty]?
 
-    func bucket(successHandler:@escaping (BeadsSet) -> (),
-                   errorHandler:@escaping (String) -> ()) -> () {
+    func bucket(successHandler:@escaping (BeadsSet) -> (), errorHandler:@escaping (String) -> ()) {
         
-        guard let jsonData = Utils.unarchiveJSON(from: "Beads - Emodji Set Sweets") else {
-            fatalError("Файл с указанным именем не найден")
-        }
-
-        do {
-            let bucketObj = try JSONDecoder().decode(BeadsSet.self, from: jsonData)
-            self.bucket = bucketObj
-            successHandler(bucketObj)
-        } catch {
-            errorHandler("Ошибка парсинга BeadsSet")
-            print("Decode Error: ", error)
-            return
+        decodeItem(BeadsSet.self, fromFile: "Beads - Emodji Set Sweets", successHandler: { (beadsSet) in
+            self.bucket = beadsSet
+            successHandler(beadsSet)
+        }) { (message) in
+            errorHandler(message)
         }
     }
     
     // Список всех возможных свойств
-    func loadBeadProperties(successHandler:@escaping ([BeadProperty]) -> (),
-                        errorHandler:@escaping (String) -> ()) {
+    func loadBeadProperties(successHandler:@escaping ([BeadProperty]) -> (), errorHandler:@escaping (String) -> ()) {
         
-        guard let jsonData = Utils.unarchiveJSON(from: "Beads - Properties") else {
-            fatalError("Файл с указанным именем не найден")
+        decodeItem([BeadProperty].self, fromFile: "Beads - Properties", successHandler: { (list) in
+            self.allBeadProperties = list
+            successHandler(list)
+        }) { (message) in
+            errorHandler(message)
         }
         
-        do {
-            let beadPropertiesList = try JSONDecoder().decode([BeadProperty].self, from: jsonData)
-            allBeadProperties = beadPropertiesList
-            successHandler(beadPropertiesList)
-        } catch {
-            errorHandler("Ошибка парсинга [BeadProperty]")
-            print("Decode Error: ", error)
-            return
-        }
     }
     
     // Список доступных наборов бусин
     func loadBeadsSets(successHandler:@escaping ([BeadsSet]) -> (),
                        errorHandler:@escaping (String) -> ()) {
         
+        
+    }
+    
+    func decodeItem<T>(_ item: T.Type, fromFile jsonFileName: String, successHandler:@escaping (T) -> (), errorHandler:@escaping (String) -> ()) where T : Decodable {
+        guard let jsonData = Utils.unarchiveJSON(from: jsonFileName) else {
+            fatalError("Файл с именем \(jsonFileName) не найден")
+        }
+        
+        do {
+            let model = try JSONDecoder().decode(item, from: jsonData)
+            successHandler(model)
+        } catch {
+            errorHandler("Ошибка парсинга \(item)")
+            print("Decode Error: ", error)
+        }
     }
     
     
